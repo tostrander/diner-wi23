d1155f7c298057ec0bf7b387c39743c0622df3d9
Tina Ostrander <tostrander@greenriver.edu>
2023-01-17 16:51:37 -0500

    Controller and home page view added

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//This is my controller
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+
+//Instantiate F3 Base class
+$f3 = Base::instance();
+
+//Define a default route (328/diner)
+$f3->route('GET /', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/diner-home.html");
+});
+
+//Run Fat Free
+$f3->run();
