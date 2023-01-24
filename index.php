643132ff21a7b531992a7f50ff78ee984786aef7
Tina Ostrander <tostrander@greenriver.edu>
2023-01-24 17:09:53 -0500

    Modified index page to display breakfast and lunch

--- index.php
+++ index.php
@@ -21,5 +21,46 @@ $f3->route('GET /', function() {
 
 });
 
+//Define a breakfast route (328/diner/breakfast)
+$f3->route('GET /breakfast', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/breakfast.html");
+
+});
+
+//Define an order1 route (328/diner/order1)
+$f3->route('GET /order1', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-form1.html");
+
+});
+
+//Define an order2 route (328/diner/order2)
+$f3->route('GET /order2', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-form2.html");
+
+});
+
+//Define a summary route (328/diner/summary)
+$f3->route('GET /order2', function() {
+
+    //Instantiate a view
+    $view = new Template();
+    echo $view->render("views/order-summary.html");
+
+});
+
+//1. Help each other get caught up
+//2. Define a lunch route + page
+//3. Add an image to breakfast and/or lunch
+
+
 //Run Fat Free
 $f3->run();
