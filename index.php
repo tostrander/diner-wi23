ddb32e491e96f3a3313a8fffb8778a4fe35eed31
Tina Ostrander <tostrander@greenriver.edu>
2023-01-24 18:08:09 -0500

    Add order form and summary page

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <?php
+// order1 route -> views/order-form1.html
+// summary route -> views/order-summary.html
 
 //This is my controller
 
 //Turn on error reporting
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
+
+//Start a session
+session_start();
 
 //Require autoload file
 require_once('vendor/autoload.php');
@@ -31,7 +36,23 @@ $f3->route('GET /breakfast', function() {
 });
 
 //Define an order1 route (328/diner/order1)
-$f3->route('GET /order1', function() {
+$f3->route('GET|POST /order1', function($f3) {
+
+    //If the form has been submitted
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $valid = true;
+
+        //If the data is valid
+        if ($valid) {
+            //Move data from POST array to SESSION array
+            $_SESSION['food'] = $_POST['food'];
+            $_SESSION['meal'] = $_POST['meal'];
+
+            //Redirect to summary page
+            $f3->reroute('summary');
+        }
+    }
 
     //Instantiate a view
     $view = new Template();
@@ -49,7 +70,7 @@ $f3->route('GET /order2', function() {
 });
 
 //Define a summary route (328/diner/summary)
-$f3->route('GET /order2', function() {
+$f3->route('GET /summary', function() {
 
     //Instantiate a view
     $view = new Template();
